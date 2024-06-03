8db8f043eb925221ec8b461e9cbc6c1e984c88bf
Ronny <103196718+Yreactives@users.noreply.github.com>
2024-06-03 23:32:09 +0700

    changes

--- design.py
+++ design.py
@@ -6,24 +6,11 @@ import pickle
 import tkinter as tk
 from tkinter import ttk
 from tkinter.filedialog import askopenfilename
-import hashlib
 import os
 from datetime import datetime, timedelta
 from meteostat import Point, Hourly, Daily
+from functions import hash_file, is_same_file
 
-def hash_file(filename):
-    """Generate a hash for the contents of a file."""
-    hasher = hashlib.md5()
-    with open(filename, 'rb') as f:
-        buf = f.read()
-        hasher.update(buf)
-    return hasher.hexdigest()
-
-def is_same_file(current_filename, previous_file_hash):
-    """Check if the current file is the same as the previous file by comparing hashes."""
-    current_file_hash = hash_file(current_filename)
-    return current_file_hash == previous_file_hash, current_file_hash
-    #return current_file_hash == previous_file_hash, current_file_hash
 
 
 def create_treeView():
---
+++ whitebox.py
@@ -0,0 +1,3 @@
+from functions import hash_file, is_same_file
+
+print(hash_file("dataset.xlsx"))
